483131117617b1b34ea3a14efc6936b4e19292fb
Sevan Golnazarian <sgolnaza@gmail.com>
2019-04-05 22:18:00 +0800

    bootstrap implementation

--- app.rb
+++ app.rb
@@ -11,9 +11,7 @@ end
 set :views, (proc { File.join(root, "app/views") })
 set :bind, '0.0.0.0'
 
-
 get '/' do
-
   erb :index # Do not remove this line
 end
 
@@ -21,22 +19,5 @@ post '/tranlsation' do
   # binding.pry
   word = params[:word]
   @translation = Parse.search(word)
-  @translation
   erb :output
 end
-# # TODO: add more routes to your app!
-# post '/new_post' do
-#   new_post = Post.new(name: params[:name], url: params[:url], votes: 0)
-#   new_post.save
-#   redirect to '/'
-# end
-#
-# post '/vote' do
-#   keys = params.keys.map { |id| id.split('_')[1].to_i }
-#   keys.each do |key|
-#     post = Post.find(key)
-#     post.votes += 1
-#     post.save
-#   end
-#   redirect to '/'
-# end
