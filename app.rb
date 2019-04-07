c36e8b31a6443a5f4274552c16f273e0d9fd3abc
Sevan Golnazarian <sgolnaza@gmail.com>
2019-04-07 21:14:48 +0800

    DB constant created in the main dir, translations table created in the schema

--- app.rb
+++ app.rb
@@ -2,13 +2,16 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+require "sqlite3"
 require_relative "app/models/parse"
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path(__dir__)
 end
 
-set :views, (proc { File.join(root, "app/views") })
+DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/translations.sqlite'))
+
+set :views, (proc { File.join(root, "views") })
 set :bind, '0.0.0.0'
 
 get '/' do
@@ -20,4 +23,9 @@ post '/tranlsation' do
   word = params[:word]
   @translation = Parse.search(word)
   erb :output
+end
+
+get "/translation-list" do
+  @translations = DB.execute("SELECT * FROM translations;")
+  erb :translations
 end
