f62e8418a3df757986b3f9c9755899b02ded5761
Sevan Golnazarian <sgolnaza@gmail.com>
2019-04-07 18:31:17 +0800

    word model changed to translation model in preparation for database implementation

--- app/models/parse.rb
+++ app/models/parse.rb
@@ -8,13 +8,14 @@ class Parse
     url = prepared_word
     url = "https://chinese.yabla.com/chinese-english-pinyin-dictionary.php?define=#{url}"
     doc = Nokogiri::HTML(open(url))
-    titles = doc.css(".pinyin")[0..1]
-    chinese = doc.css(".word")[0..1]
+    titles = doc.css('.pinyin')[0..1]
+    chinese = doc.css('.word')[0..1]
     chinese_first = chinese[0].text.strip
     chinese_second = chinese[1].text.strip
     first = titles[0].text
     second = titles[1].text
-    {first: first, second: second, chinese_first: chinese_first, chinese_second: chinese_second }
+    { first: first, second: second, chinese_first: chinese_first,
+      chinese_second: chinese_second, english_word: word }
   end
 end
 
--- app/models/word.rb
+++
@@ -1,7 +0,0 @@
-# base class for words
-class Word
-  attr_accessor :word
-  def initialize(base_word)
-    @word = base_word
-  end
-end
